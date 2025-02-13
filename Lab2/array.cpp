d2983effb7740ad72c45d769928e808adbaee114
Brendan-Han <bjhan@cpp.edu>
2025-02-13 14:40:25 -0800

    complete update to rename lab2

--- Lab2/array.cpp
+++ Lab2/array.cpp
@@ -10,7 +10,6 @@ using namespace std;
 void printMemArr(const int * arr, int size) {
     printf("Array - Each int is worth %lu bytes\n", sizeof(arr[0]));
     for(int i = 0; i < size; i++) {
-        
         printf("Value :%i at Memory Location: %p\n", arr[i], arr + i);
     }
 
