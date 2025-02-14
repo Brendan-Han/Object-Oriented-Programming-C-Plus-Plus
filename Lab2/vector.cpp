146562fdbfd18b0f0f04226c0c236115b412c55d
Brendan-Han <bjhan@cpp.edu>
2025-02-13 16:45:30 -0800

    variable

--- Lab2/vector.cpp
+++ Lab2/vector.cpp
@@ -6,37 +6,37 @@ using namespace std;
  * 
  * @param size - the number of elements in the array
  */
-void printMemVec(vector<int> &vec, int size) {
+void printMemVec(vector<int> &vec, int SIZE) {
     for(int i = 0; i < vec.size(); i++) {
         printf("Value :%i at Memory Location: %p\n", vec[i], &vec[i]);
     }
 
 }
 
-void incVecBy10(vector<int> &vec, int size) {
-    for(int i = 0; i < size; i++) {
+void incVecBy10(vector<int> &vec, int SIZE) {
+    for(int i = 0; i < SIZE; i++) {
         vec[i] += 10; 
     }
 }
 
 int main() {
-    const int size = 5;
-    vector<int> vec(size);
-    for(int i = 0; i < size; i++) {
+    const int SIZE = 5;
+    vector<int> vec(SIZE);
+    for(int i = 0; i < SIZE; i++) {
         vec[i] = 100 + i;
     }
 printf("Before Increment------------\n");
-printMemVec(vec, size);
-incVecBy10(vec,size);
+printMemVec(vec, SIZE);
+incVecBy10(vec,SIZE);
 printf("After Increment------------\n");
-printMemVec(vec,size);
+printMemVec(vec,SIZE);
 vec.pop_back();
 printf("After Pop------------\n");
-printMemVec(vec,size);
+printMemVec(vec,SIZE);
 vec.push_back(101);
 vec.push_back(102);
 printf("After Push------------\n");
-printMemVec(vec,size);
+printMemVec(vec,SIZE);
 
 return 0;
 }
