8b81cbd9364fd456a8b1af2fad6f56fc9709d537
Brendan-Han <bjhan@cpp.edu>
2025-02-23 15:08:46 -0800

    update

---
+++ Comprehension1/pet.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#ifndef PET_H
+#define PET_H
+using namespace std;
+
+class pet {
+    pet ();
+    pet (string name, int age, string owner, bool isHouseTrained);
+
+    //mutator functions
+    void updateName (string name);
+    void updateAge (int age);
+    void updateOwner (string owner);
+    void updateIsHouseTrained (bool isHouseTrained);
+
+    //accessor functions
+    string getName() const;
+    int getAge() const;
+    string getOwner() const;
+    bool getIsHouseTrained() const;
+
+private:
+    string name;
+    int age;
+    string owner;
+    bool isHouseTrained;    
+};
+#endif
