89dacecf0e103054cdcba6a0f0ba0dadeef88854
Brendan-Han <bjhan@cpp.edu>
2025-03-01 13:03:41 -0800

    Update RPG.h

---
+++ Lab3/RPG.h
@@ -0,0 +1,41 @@
+#ifndef RPG_H
+#define RPG_H
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+const int SKILL_SIZE = 2;
+
+class RPG {
+public:
+
+// constructors
+RPG();
+RPG(string name, int health, int strength, int defense, string type);
+
+//mutators
+void setSkills() const;
+void printAction(string, RPG) const;
+void updateHealth(int) const;
+void attack(RPG *) const;
+void useSkill(RPG *) const;
+
+//accessor
+const bool isAlive();
+const string getName();
+const int getHealth();
+const int getStrength();
+const int getDefense();
+
+private:
+
+string name;
+int health;
+int strength;
+int defense;
+string typ;
+string skills[SKILL_SIZE];
+
+};
+#endif
