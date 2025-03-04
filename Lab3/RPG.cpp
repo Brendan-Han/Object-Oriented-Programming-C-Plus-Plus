23c308203413c02eca53c77c31bd993defdfe920
Brendan-Han <bjhan@cpp.edu>
2025-03-03 17:19:44 -0800

    Update RPG.cpp

--- Lab3/RPG.cpp
+++ Lab3/RPG.cpp
@@ -4,3 +4,23 @@
 #include "RPG.h"
 using namespace std;
 
+RPG::RPG(){
+
+    name = "NPC";
+    health = 100;
+    strength = 10;
+    defense = 10;
+    type = "warrior";
+    skills[0] = "slash";
+    skills[1] = "parry";
+}
+
+RPG::RPG(string name, int health, int strength, int defense, string type){
+
+    this->name = name;
+    this->health = health;
+    this->strength = strength;
+    this->defense = defense;
+    this->type = type;
+
+}
