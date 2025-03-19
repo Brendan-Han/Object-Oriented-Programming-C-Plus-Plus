f7ae5e8c262e0cf519a90992030e1dcdf0316801
Brendan-Han <bjhan@cpp.edu>
2025-03-19 13:21:46 -0700

    update Lab4

---
+++ Lab4/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "RPG.h"
+using namespace std;
+
+//unused functions: attack, useSkill
+
+int main(){
+    RPG RPG1;
+    RPG RPG2("John", 100, 10, 10, "mage");
+
+
+    cout << "Name: " << RPG1.getName() << endl << "Health: " << RPG1.getHealth() << endl << "Str: " << RPG1.getStrength() << endl << "Def: " << RPG1.getDefense() << endl;
+
+    RPG1.updateHealth(1000);
+    
+    cout << "New Health: " << RPG1.getHealth() << endl << "Alive? " << (RPG1.isAlive()? "Yes":"No") << endl;
+
+    cout << "Name: " << RPG2.getName() << endl << "Health: " << RPG2.getHealth() << endl << "Str: " << RPG2.getStrength() << endl << "Def: " << RPG2.getDefense() << endl;
+
+    RPG2.updateHealth(1000);
+    
+    cout << "New Health: " << RPG2.getHealth() << endl << "Alive? " << (RPG2.isAlive()? "Yes":"No");
+
+}
